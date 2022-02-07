5abce864826cd70e7652f418ed8b632946c387e2
NethraCR <98078370+NethraCR@users.noreply.github.com>
2022-02-07 12:41:50 +0530

    Add files via upload

---
+++ Conditional.py
@@ -0,0 +1,20 @@
+num=3
+if num>0:
+    print(num,"Ïs a positive no.")
+print("I am outside block")
+
+num=-1
+if num>0:
+    print(num,"is a positive no")
+print("i am outside block")
+
+num1=1
+num2=8
+num3=5
+
+if num1>num2 and num1>num3:
+    print(num1)
+elif num2>num3:
+    print(num2)
+else:
+    print("number 3 is the largest",num3)
